4e20fac3bbf319243a7c92164497ba984a37e059
Idemili Ifechukwu Anthony <119919902+Anthony7145@users.noreply.github.com>
2023-10-18 10:04:51 -0600

    Add files via upload

--- cprg304complexityandsorting/Complexity and Sorting/src/SortingUtility.java
+++ cprg304complexityandsorting/Complexity and Sorting/src/SortingUtility.java
@@ -1,4 +1,128 @@
-
+// Define the sorting utility class
 public class SortingUtility {
+	static bubbleSort(arr) {
+	    // Implement Bubble Sort
+	  }
 
+	  static insertionSort(arr) {
+	    // Implement Insertion Sort
+	  }
+
+	  static selectionSort(arr) {
+	    // Implement Selection Sort
+	  }
+
+	  static mergeSort(arr) {
+	    // Implement Merge Sort
+	  }
+
+	  static quickSort(arr) {
+	    // Implement Quick Sort
+	  }
+
+	  static customSort(arr) {
+	    // Implement your custom sorting algorithm
+	  }
+	}
+
+	// Read shapes from a file and create an array of shape objects
+	function readShapesFromFile(filename) {
+	  try {
+	    const data = fs.readFileSync(filename, 'utf8');
+	    const lines = data.split('\n');
+	    const shapeArray = [];
+
+	    // Parse lines and create shape objects
+	    for (let i = 1; i < lines.length; i++) {
+	      const line = lines[i].trim().split(' ');
+	      const shapeType = line[0];
+	      const height = parseFloat(line[1]);
+
+	      if (shapeType === 'Cylinder') {
+	        const radius = parseFloat(line[2]);
+	        shapeArray.push(new Cylinder(height, radius));
+	      }
+	      // Implement parsing for other shape types (Cone, Pyramid, Prism)
+	    }
+
+	    return shapeArray;
+	  } catch (err) {
+	    console.error('Error reading the file:', err);
+	    process.exit(1);
+	  }
+	}
+
+	// Benchmark sorting algorithms and measure execution time
+	function benchmarkSortingAlgorithms(shapes, algorithmName) {
+	  const start = process.hrtime();
+	  switch (algorithmName) {
+	    case 'Bubble':
+	      SortingUtility.bubbleSort(shapes);
+	      break;
+	    case 'Insertion':
+	      SortingUtility.insertionSort(shapes);
+	      break;
+	    case 'Selection':
+	      SortingUtility.selectionSort(shapes);
+	      break;
+	    case 'Merge':
+	      SortingUtility.mergeSort(shapes);
+	      break;
+	    case 'Quick':
+	      SortingUtility.quickSort(shapes);
+	      break;
+	    case 'Custom':
+	      SortingUtility.customSort(shapes);
+	      break;
+	    default:
+	      console.error('Invalid sorting algorithm');
+	      process.exit(1);
+	  }
+	  const end = process.hrtime(start);
+	  const executionTime = (end[0] * 1e9 + end[1]) / 1e6; // in milliseconds
+	  console.log(`Sorting with ${algorithmName} took ${executionTime} ms`);
+	}
+
+	// Command-line argument parsing
+	function parseCommandLineArguments() {
+	  const args = process.argv.slice(2);
+	  if (args.length !== 6 || args[0] !== '-f' || args[2] !== '-t' || args[4] !== '-s') {
+	    console.error('Invalid command-line arguments. Usage: -f <filename> -t <comparisonType> -s <sortingAlgorithm>');
+	    process.exit(1);
+	  }
+	  const filename = args[1];
+	  const comparisonType = args[3].toLowerCase();
+	  const sortingAlgorithm = args[5].toLowerCase();
+
+	  return { filename, comparisonType, sortingAlgorithm };
+	}
+
+	// Main function
+	function main() {
+	  const { filename, comparisonType, sortingAlgorithm } = parseCommandLineArguments();
+	  const shapes = readShapesFromFile(filename);
+
+	  switch (comparisonType) {
+	    case 'h':
+	      shapes.sort((a, b) => a.compareTo(b));
+	      break;
+	    case 'v':
+	      shapes.sort(GeometricShape.compareByVolume);
+	      break;
+	    case 'a':
+	      shapes.sort(GeometricShape.compareByBaseArea);
+	      break;
+	    default:
+	      console.error('Invalid comparison type');
+	      process.exit(1);
+	  }
+
+	  benchmarkSortingAlgorithms(shapes.slice(), sortingAlgorithm);
+	  // Clone the shapes array before benchmarking customSort
+	  if (sortingAlgorithm === 'custom') {
+	    benchmarkSortingAlgorithms(shapes.slice(), sortingAlgorithm);
+	  }
+	}
+
+	main();
 }
